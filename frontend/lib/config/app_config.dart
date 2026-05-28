310edcdd00c10ad2a6d21f515395fe8852225ea6
Gratiudi <tinbiteyonas97@gmail.com>
2026-05-27 19:07:52 -0700

    AppConfig and provider/import wiring fixes

---
+++ frontend/lib/config/app_config.dart
@@ -0,0 +1,12 @@
+class AppConfig {
+  static const String baseUrl = 'http://localhost:3000/api';
+  static const String apiBaseUrl = 'http://localhost:3000/api';
+  static const String employeesEndpoint = '/employees';
+  static const String shiftsEndpoint = '/shifts';
+  static const String attendanceEndpoint = '/attendance';
+  static const String registerEndpoint = '/register';
+  static const String loginEndpoint = '/login';
+  static const String logoutEndpoint = '/logout';
+  static const String assignShiftEndpoint = '/assignShift';
+  static const String updateShiftEndpoint = '/updateShift';
+}
